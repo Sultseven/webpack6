3bfad379641957e5e8e01dbd965020b78cf35da3
Sultcon1 <sult.seven17@mail.ru>
2021-06-22 19:41:10 +0300

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,8 @@ import './modules/twoModule';
 import '../styles/main.scss'
 
 console.log('index js');
+
+
+var checkbox = document.querySelector("#purple");
+checkbox.addEventListener("change", function(){document.body.style.background = checkbox.checked ? "mediumpurple" : "";});
+
